1c651a5295cd91c7705303debc80a37dbd0d049f
11410100142 <whitefanghima@gmail.com>
2015-04-23 00:27:53 +0700

    Create Array1Dmhs.java

---
+++ Array1Dmhs.java
@@ -0,0 +1,58 @@
+import java.io.*;
+
+
+public class Array1Dmhs {
+
+    public static void main (String [] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+    			
+  
+		System.out.print("Masukan jumlah mahasiswa =");
+		int N = Integer.parseInt(inputan.nextLine());
+    	
+    	int [] nilaiMhs = new int[5];
+    	String [] namaMhs = new String[5];
+    	
+    	System.out.print("Nama Mahasiswa :");
+    	namaMhs[0] = Inputan.nextLine();
+    	
+    	System.out.print("Nilai Mahasiswa :");
+    	nilaiMhs[0]=Integer.parseInt(Inputan.nextLine());
+    	
+    	
+    	System.out.print("Nama Mahasiswa :");
+    	namaMhs[1] = Inputan.nextLine();
+    	
+    	System.out.print("Nilai Mahasiswa :");
+    	nilaiMhs[1]=Integer.parseInt(Inputan.nextLine());
+    	
+    	System.out.print("Nama Mahasiswa :");
+    	namaMhs[2] = Inputan.nextLine();
+    	
+    	System.out.print("Nilai Mahasiswa :");
+    	nilaiMhs[2]=Integer.parseInt(Inputan.nextLine());
+    	
+    	for(int i=0; i<5; i++){
+    			
+
+    		
+    		}
+    		System.out.println("+------------------------------------------------------------+");
+    		System.out.println("| nama mahasiswa | nilai mahasiswa 		| nilai ips mahasiswa|");
+    		System.out.println("+------------------------------------------------------------+");
+    		System.out.println("nama MHS ke 1="+namaMhs[0]);
+    		System.out.println("nilai MHS ke 1="+nilaiMhs[0]);
+    		System.out.println("nama MHS ke 1="+namaMhs[1]);
+    		System.out.println("nilai MHS ke 1="+nilaiMhs[1]);
+    		System.out.println("nama MHS ke 1="+namaMhs[2]);
+    		System.out.println("nilai MHS ke 1="+nilaiMhs[2]);
+    		System.out.println("nama MHS ke 1="+namaMhs[3]);
+    		System.out.println("nilai MHS ke 1="+nilaiMhs[3]);
+    		System.out.println("nama MHS ke 1="+namaMhs[4]);
+    		System.out.println("nilai MHS ke 1="+nilaiMhs[4]);
+    	
+    }
+    
+    
+}
